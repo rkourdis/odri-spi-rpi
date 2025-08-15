87fc376258a6f2fe1cff4286565e442fff33e6a4
Rafael Kourdis <rafael.kourdis@gmail.com>
2025-08-15 21:15:36 +0200

    Use multiprocessing to do parallel SPI

--- src/odri_spi_rpi.py
+++ src/odri_spi_rpi.py
@@ -6,187 +6,190 @@
 
 # Modified for linear control and multiple SPIs - Rafael Kourdis
 
+import math
 import copy
 import time
 import struct
 import spidev
+import statistics
 
 from math import pi
-from itertools import chain
+from termcolor import colored
+import multiprocessing as multiproc
 
 def crc32(buf):
-  crc=0xffffffff
+    crc=0xffffffff
 
-  for val in buf:
-    crc ^= val << 24
-    for _ in range(8):
-      crc = crc << 1 if (crc & 0x80000000) == 0 else (crc << 1) ^ 0x104c11db7
-  
-  return crc
+    for val in buf:
+        crc ^= val << 24
+        for _ in range(8):
+            crc = crc << 1 if (crc & 0x80000000) == 0 else (crc << 1) ^ 0x104c11db7
+
+    return crc
 
 def checkcrc(buf):
-  crc = crc32(buf[:-4])
-  return (crc & 0xffff == buf[-4] * 256 + buf[-3] and (crc & 0xffff0000) >> 16 == buf[-2] * 256 + buf[-1])
+    crc = crc32(buf[:-4])
+    return (crc & 0xffff == buf[-4] * 256 + buf[-3] and (crc & 0xffff0000) >> 16 == buf[-2] * 256 + buf[-1])
 
 class SPIuDriver:
-  def __init__(self, connected_boards = 1, waitForInit = True, absolutePositionMode = False, spi_bus = 0):
-    self.connected_boards = connected_boards
+    def __init__(self, connected_boards = 1, waitForInit = True, absolutePositionMode = False, spi_bus = 0):
+        self.connected_boards = connected_boards
 
-    # On RPi5, we have to use https://github.com/waveform80/rpi-lgpio
-    # instead of RPi.GPIO.
-    
-    # We rely on the hardware for CS. It seems that the delay for the uDriver DMA
-    # to transfer the sensor packet is enough.
+        # On RPi5, we have to use https://github.com/waveform80/rpi-lgpio
+        # instead of RPi.GPIO.
 
-    #Initialise SPI
-    self.spi = spidev.SpiDev()
-    self.spi.open(spi_bus, 0)
-    self.spi.mode=0
-    self.spi.max_speed_hz = 8000000
+        # We rely on the hardware for CS. It seems that the delay for the uDriver DMA
+        # to transfer the sensor packet is enough.
 
-    # Allocate all variables
-    self.is_system_enabled = 0
-    self.error_code = 0
+        #Initialise SPI
+        self.spi = spidev.SpiDev()
+        self.spi.open(spi_bus, 0)
+        self.spi.mode=0
+        self.spi.max_speed_hz = 8000000
 
-    self.position    = [0, 0]
-    self.velocity    = [0, 0]
-    self.current     = [0, 0]    
-    self.is_enabled  = [0, 0]    
-    self.is_ready    = [0, 0]
-    self.iSatCurrent = [5.0, 5.0]
-    
-    self.has_index_been_detected = [0, 0]
-    self.index_toggle_bit        = [0, 0]
-    self.EIOC                    = [1] * 2 if absolutePositionMode else [0] * 2
+        # Allocate all variables
+        self.is_system_enabled = 0
+        self.error_code = 0
 
-    self.alpha = [0.] * (2 * 4 * self.connected_boards)
-    self.beta = [0., 0.]
-   
-    self.timeout = 0
-    self.error = -1
+        self.position    = [0, 0]
+        self.velocity    = [0, 0]
+        self.current     = [0, 0]
+        self.is_enabled  = [0, 0]
+        self.is_ready    = [0, 0]
+        self.iSatCurrent = [5.0, 5.0]
 
-    # Wait for system enable:
-    if waitForInit:
-      print(">> Calibrating motor, please wait")
-      while(not self.is_ready[0]):
-        self.transfer()
-        time.sleep(0.001)
-    
-    if absolutePositionMode:
-      if (not self.has_index_been_detected[0] or not self.has_index_been_detected[1]):
-          print(">> Waiting for index pulse to have absolute position reference, please move the motors manualy")
-          displayedIndex0 = False
-          displayedIndex1 = False
-          
-          while(not self.has_index_been_detected[0] or not self.has_index_been_detected[1]):
-            self.transfer()
-            if self.has_index_been_detected[0] == True and displayedIndex0 == False:
-                print (" >> Index 0 detected!")
-                displayedIndex0 = True
+        self.has_index_been_detected = [0, 0]
+        self.index_toggle_bit        = [0, 0]
+        self.EIOC                    = [1] * 2 if absolutePositionMode else [0] * 2
 
-            if self.has_index_been_detected[1] == True and displayedIndex1 == False:
-                print (" >> Index 1 detected!")
-                displayedIndex1 = True
-            
-            time.sleep(0.001)
+        self.alpha = [0.] * (2 * 4 * self.connected_boards)
+        self.beta  = [0., 0.]
 
-    print ("Ready!")
+        self.timeout = 0
+        self.error   = -1
 
-  def transfer(self):
-    # Generate command packet
-    ES = 1
-    EM1 = 1	
-    EM2	= 1
-    EPRE = 1
-    EI1OC = self.EIOC[0]
-    EI2OC	= self.EIOC[1]
-    mode = (ES << 7) | (EM1 << 6) | (EM2<<5)|(EPRE<<4)|(EI1OC<<3)|(EI2OC<<2)
-    timeout = self.timeout
+        # Wait for system enable:
+        if waitForInit:
+            print(">> Calibrating motor, please wait")
+            while(not self.is_ready[0]):
+                self.transfer()
+                time.sleep(0.001)
 
-    rawIsat0 = int(self.iSatCurrent[0] * (1 << 3))
-    rawIsat1 = int(self.iSatCurrent[1] * (1 << 3))
+        if absolutePositionMode:
+            if (not self.has_index_been_detected[0] or not self.has_index_been_detected[1]):
+                print(">> Waiting for index pulse to have absolute position reference, please move the motors manualy")
+                displayedIndex0 = False
+                displayedIndex1 = False
 
-    header_values = [
-        # Mode + Timeout 16 bits:
-        mode,
-        timeout,
+                while(not self.has_index_been_detected[0] or not self.has_index_been_detected[1]):
+                    self.transfer()
+                    if self.has_index_been_detected[0] == True and displayedIndex0 == False:
+                        print (" >> Index 0 detected!")
+                        displayedIndex0 = True
 
-        # Saturation current 0 + 1 uint16_t:
-        rawIsat0,
-        rawIsat1,
+                    if self.has_index_been_detected[1] == True and displayedIndex1 == False:
+                        print (" >> Index 1 detected!")
+                        displayedIndex1 = True
 
-        # Index (uint16_t):
-        0,
-     ]
+                    time.sleep(0.001)
 
-    # We pack these values in big-endian *byte* mode. The C2000
-    # is little-endian, but for _words_ of 16 bits. The header does
-    # not contain any 32 bit values. 
-    header_bytes = struct.pack("> BB BB H", *header_values)
+        print ("Ready!")
 
-    # Floats should be packed in big-endian byte mode,
-    # but we'll need to manually flip the words so that we get
-    # them in little-endian word mode:
-    floats = self.alpha + self.beta
-    
-    floats_bytes = struct.pack(f"> {2 * 4 * self.connected_boards + 2}f", *floats)
-    floats_word_swapped = b''.join(
-      floats_bytes[f_idx*4 + 2 : f_idx*4 + 4] + floats_bytes[f_idx*4 + 0 : f_idx*4 + 2]
-      for f_idx in range(len(floats))
-    )
+    def transfer(self):
+        # Generate command packet
+        ES      = 1
+        EM1     = 1
+        EM2	    = 1
+        EPRE    = 1
+        EI1OC   = self.EIOC[0]
+        EI2OC	= self.EIOC[1]
+        mode    = (ES << 7) | (EM1 << 6) | (EM2<<5)|(EPRE<<4)|(EI1OC<<3)|(EI2OC<<2)
+        timeout = self.timeout
 
-    commandPacket = bytearray(
-      header_bytes +
-      b'\x00' * (len(header_bytes) % 4) +  # 32-bit values are aligned on 32-bit boundaries
-      floats_word_swapped +
-      struct.pack(f"I", 0)               # Temporary CRC (uint32_t)
-    )
-    
-    crc=crc32(commandPacket[:-4])
-    commandPacket[-4]=(crc>>24)&0xff
-    commandPacket[-3]=(crc>>16)&0xff
-    commandPacket[-2]=(crc>>8)&0xff
-    commandPacket[-1]=(crc)&0xff
+        rawIsat0 = int(self.iSatCurrent[0] * (1 << 3))
+        rawIsat1 = int(self.iSatCurrent[1] * (1 << 3))
 
-    # Trim sensor packet to 17 words due to the fact that the SPI TX buffer
-    # is limited and bytes sent after (because the command is larger) will be garbage: 
-    sensorPacket = bytearray(self.spi.xfer(commandPacket))[:34]
+        header_values = [
+                # Mode + Timeout 16 bits:
+                mode,
+                timeout,
 
-    # print("Command: ", " ".join(format(x, "02x") for x in commandPacket))
-    # print("Sensor:  ", " ".join(format(x, "02x") for x in sensorPacket))
-    # print(checkcrc(sensorPacket))
-    # print()
+                # Saturation current 0 + 1 uint16_t:
+                rawIsat0,
+                rawIsat1,
 
-    if not checkcrc(sensorPacket):
-      raise Exception(f"Error: Corrupted sensor frame - is uDriver powered on?")
+                # Index (uint16_t):
+                0,
+         ]
 
-    # Decode received sensor packet
-    data = struct.unpack(">H H i i h h h h xxxxxxxxxxxxxx", sensorPacket)
-    self.is_system_enabled          = data[0]&0b1000000000000000 != 0
-    self.is_enabled[0]              = data[0]&0b0100000000000000 != 0
-    self.is_ready[0]                = data[0]&0b0010000000000000 != 0
-    self.is_enabled[1]              = data[0]&0b0001000000000000 != 0
-    self.is_ready[1]                = data[0]&0b0000100000000000 != 0
-    self.has_index_been_detected[0] = data[0]&0b0000010000000000 != 0
-    self.has_index_been_detected[1] = data[0]&0b0000001000000000 != 0
+        # We pack these values in big-endian *byte* mode. The C2000
+        # is little-endian, but for _words_ of 16 bits. The header does
+        # not contain any 32 bit values.
+        header_bytes = struct.pack("> BB BB H", *header_values)
 
-    self.error             = data[0]&0b0000000000001111
-    self.position[0] = data[2] / (1<<24) * 2.0 * pi
-    self.position[1] = data[3] / (1<<24) * 2.0 * pi
-    self.velocity[0] = data[4] / (1<<11) * 2000*pi/60.0
-    self.velocity[1] = data[5] / (1<<11) * 2000*pi/60.0
-    self.current[0]  = data[6]  / (1<<10)
-    self.current[1]  = data[7]  / (1<<10)
-    
-    if self.error!=0:
-        raise Exception(f"Error from motor driver: Error {self.error}")
-   
-  def stop(self):
+        # Floats should be packed in big-endian byte mode,
+        # but we'll need to manually flip the words so that we get
+        # them in little-endian word mode:
+        floats = self.alpha + self.beta
+
+        floats_bytes = struct.pack(f"> {2 * 4 * self.connected_boards + 2}f", *floats)
+        floats_word_swapped = b''.join(
+            floats_bytes[f_idx*4 + 2 : f_idx*4 + 4] + floats_bytes[f_idx*4 + 0 : f_idx*4 + 2]
+            for f_idx in range(len(floats))
+        )
+
+        commandPacket = bytearray(
+            header_bytes +
+            b'\x00' * (len(header_bytes) % 4) +    # 32-bit values are aligned on 32-bit boundaries
+            floats_word_swapped +
+            struct.pack(f"I", 0)                   # Temporary CRC (uint32_t)
+        )
+
+        crc = crc32(commandPacket[:-4])
+        commandPacket[-4] =(crc>>24) & 0xff
+        commandPacket[-3] =(crc>>16) & 0xff
+        commandPacket[-2] =(crc>>8) & 0xff
+        commandPacket[-1] =(crc) & 0xff
+
+        # Trim sensor packet to 17 words due to the fact that the SPI TX buffer
+        # is limited and bytes sent after (because the command is larger) will be garbage:
+        sensorPacket = bytearray(self.spi.xfer(commandPacket))[:34]
+
+        # print("Command: ", " ".join(format(x, "02x") for x in commandPacket))
+        # print("Sensor:    ", " ".join(format(x, "02x") for x in sensorPacket))
+        # print(checkcrc(sensorPacket))
+        # print()
+
+        if not checkcrc(sensorPacket):
+            raise Exception(f"Error: Corrupted sensor frame - is uDriver powered on?")
+
+        # Decode received sensor packet
+        data = struct.unpack(">H H i i h h h h xxxxxxxxxxxxxx", sensorPacket)
+        self.is_system_enabled          = data[0]&0b1000000000000000 != 0
+        self.is_enabled[0]              = data[0]&0b0100000000000000 != 0
+        self.is_ready[0]                = data[0]&0b0010000000000000 != 0
+        self.is_enabled[1]              = data[0]&0b0001000000000000 != 0
+        self.is_ready[1]                = data[0]&0b0000100000000000 != 0
+        self.has_index_been_detected[0] = data[0]&0b0000010000000000 != 0
+        self.has_index_been_detected[1] = data[0]&0b0000001000000000 != 0
+
+        self.error       = data[0]&0b0000000000001111
+        self.position[0] = data[2] / (1<<24) * 2.0 * pi
+        self.position[1] = data[3] / (1<<24) * 2.0 * pi
+        self.velocity[0] = data[4] / (1<<11) * 2000*pi/60.0
+        self.velocity[1] = data[5] / (1<<11) * 2000*pi/60.0
+        self.current[0]  = data[6] / (1<<10)
+        self.current[1]  = data[7] / (1<<10)
+
+        if self.error!=0:
+            raise Exception(f"Error from motor driver: Error {self.error}")
+
+    def stop(self):
         self.EIOC[0] = self.EIOC[1] = 0
 
         self.alpha = [0.] * (2 * 4 * self.connected_boards)
-        self.beta = [0., 0.]
-   
+        self.beta  = [0., 0.]
+
         self.iSatCurrent[0] = self.iSatCurrent[1] = 0
         self.timeout = 0
 
@@ -195,7 +198,114 @@ class SPIuDriver:
 
         for _ in range(2):
             self.transfer()
-            
+
             t += dt
             while(time.perf_counter() < t):
                 pass
+
+class SharedCommand:
+    def __init__(self, connected_boards: int):
+        self.alpha       = multiproc.RawArray("f",  2 * 4 * connected_boards)
+        self.beta        = multiproc.RawArray("f",  2)
+        self.iSatCurrent = multiproc.RawArray("f",  2)
+
+        self.lock = multiproc.Lock()
+
+class BackgroundSPIuDriver:
+    @classmethod
+    def _subprocess_loop(_, dt, ready_flag, quit_flag, state, command, spi_args,):
+        ud = SPIuDriver(**spi_args)
+        ready_flag.value = True
+
+        LOOP_TIME_HIST_SZ = 100
+        cur_loop_time_idx = -1
+        loop_time_hist = [math.nan] * LOOP_TIME_HIST_SZ 
+
+        _outer_quit = False
+
+        try:
+            t_loop = time.perf_counter()
+
+            while True:
+                if quit_flag.value:
+                    _outer_quit = True
+                    raise Exception()
+                
+                # This block should ideally not take long, just
+                # until the controller finishes writing the command:
+                with command.lock:
+                    ud.alpha       = command.alpha[:]
+                    ud.beta        = command.beta[:]
+                    ud.iSatCurrent = command.iSatCurrent[:]
+
+                ud.transfer()
+
+                x_current = ud.position + ud.velocity
+
+                # Same for this:
+                with state.get_lock():
+                    state[:] = x_current
+
+                cur_loop_time_idx += 1
+                if (cur_loop_time_idx == LOOP_TIME_HIST_SZ):
+                    cur_loop_time_idx = 0
+
+                loop_time_hist[cur_loop_time_idx] = time.perf_counter() - t_loop
+
+                t_loop += dt
+                while time.perf_counter() < t_loop:
+                    pass
+
+        except (KeyboardInterrupt, Exception) as ex:
+            ud.stop()
+
+            print(
+                colored(
+                    f"Average SPI {spi_args['spi_bus']} loop time (ms): {statistics.mean(loop_time_hist) * 1e+3 :.2f}",
+                    "yellow"
+                )
+            )
+
+            if not _outer_quit and not isinstance(ex, KeyboardInterrupt):
+                raise
+
+    def __init__(self, dt, **kwargs):
+        self._quit = multiproc.Value('b', False)
+        ready_flag = multiproc.Value('b', False)
+
+        self._state = multiproc.Array('f', 4)
+        self.command = SharedCommand(kwargs["connected_boards"])
+
+        self.proc = multiproc.Process(
+            target = self._subprocess_loop,
+            kwargs = {
+                "dt": dt,
+                "spi_args": kwargs,
+                "ready_flag": ready_flag,
+                "quit_flag": self._quit,
+                "command": self.command,
+                "state": self._state,
+            }
+        )
+
+        self.proc.start()
+
+        # Block until the board is ready:
+        while True:
+            if not self.alive:
+                raise Exception("Could not initialize board!")
+            
+            if ready_flag.value:
+                break
+            
+    @property
+    def alive(self):
+        return self.proc.is_alive()
+    
+    @property
+    def state(self):
+        with self._state.get_lock():
+            return list(self._state)
+
+    def stop(self):
+        self._quit.value = True
